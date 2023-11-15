9691e5f615cb182ae2e29e51a75c1f3c9bd2b360
ibertran <128724498+ibtrd@users.noreply.github.com>
2023-11-15 19:09:59 +0100

    Rework memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:22:59 by ibertran          #+#    #+#             */
-/*   Updated: 2023/11/09 12:00:49 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2023/11/15 18:49:53 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	if (dest == src || n == 0)
-		return (dest);
 	if (dest < src)
-		ft_memcpy(dest, src, n);
-	else
+		return (ft_memcpy(dest, src, n));
+	while (n > 0)
 	{
-		while (n > 0)
-		{
-			((char *)dest)[n - 1] = ((char *)src)[n - 1];
-			n--;
-		}
+		((char *)dest)[n - 1] = ((char *)src)[n - 1];
+		n--;
 	}
 	return (dest);
 }
