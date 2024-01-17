61e53e67a88df75d419f2abb81a7adfa6b5bfcbc
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-01-17 20:00:49 +0100

    Replace vector div/mult with bitshifting

--- srcs/vector/ft_vector_add.c
+++ srcs/vector/ft_vector_add.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 23:26:30 by ibertran          #+#    #+#             */
-/*   Updated: 2024/01/15 16:34:34 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/01/17 19:24:04 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_vector_add(t_vector *v, void *item)
 		return (status);
 	if (v->total == v->capacity)
 	{
-		status = ft_vector_resize(v, v->capacity * 2);
+		status = ft_vector_resize(v, v->capacity << 1);
 		if (status != UNDEFINE)
 		{
 			ft_memcpy(v->items + v->item_size * v->total, item, v->item_size);
--- srcs/vector/ft_vector_delete.c
+++ srcs/vector/ft_vector_delete.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 00:57:48 by ibertran          #+#    #+#             */
-/*   Updated: 2024/01/15 02:02:21 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/01/17 19:24:58 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ int	ft_vector_delete(t_vector *v, size_t index)
 		end = ft_vector_get(v, v->total - 1);
 		ft_memcpy2(start, start + v->item_size, end);
 		v->total--;
-		if (v->total == v->capacity / 4)
-			ft_vector_resize(v, v->capacity / 2);
+		if (v->total == v->capacity >> 2)
+			ft_vector_resize(v, v->capacity >> 2);
 	}
 	return (status);
 }
