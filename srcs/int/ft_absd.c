071a339eeacf773ad0471b0868b31ab0e89096c4
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-01-19 06:14:37 +0100

    Add ft_absd()

--- srcs/int/ft_absd.c
+++ srcs/int/ft_absd.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_integer.h                                    :+:      :+:    :+:   */
+/*   ft_absd.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 02:58:54 by ibertran          #+#    #+#             */
-/*   Updated: 2024/01/18 01:16:42 by ibertran         ###   ########lyon.fr   */
+/*   Created: 2024/01/19 06:11:52 by ibertran          #+#    #+#             */
+/*   Updated: 2024/01/19 06:14:12 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_INTEGER_H
-# define FT_INTEGER_H
-
-int		ft_strtoi(const char *ptr, char **endptr);
-long	ft_strtol(const char *ptr, char **endptr);
-int		ft_abs(int x);
-double	ft_absd(double x);
-
-#endif
+double	ft_absd(double x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
+}
